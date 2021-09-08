ea416f930c5db7b6f91af879653126e280df1657
originlimit <90251333+originlimit@users.noreply.github.com>
2021-09-08 10:05:48 -0700

    fizzbuzz challenge with array and ternary operator

---
+++ fizzbuzz.js
@@ -0,0 +1,8 @@
+
+function fizzBuzz(ar){
+    for(let i = 1; i <= 100; i++){
+        i % 5 == 0 && i % 3 == 0 ? ar.push("FIZZBUZZ") : i % 5 == 0 ? ar.push("buzz") : i % 3 == 0 ? ar.push("fizz") : ar.push(i);
+    }
+    return ar;
+}
+
